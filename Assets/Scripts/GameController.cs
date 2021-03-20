8e8bef38710b586ed8f9263a53df81315e6c6628
Dion Pimentel <dionpimentel260@gmail.com>
2021-03-20 16:55:26 -0700

    Scene Changes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private bool firstItem;
+    private bool secondItem;
+    private bool thirdItem;
+    private bool fourthItem;
+    private bool fifthItem;
+    private bool sixthItem;
+
+    public GameObject roomObjects;
+
+    private AudioSource bgm;
+    private AudioSource dialogue;
+
+
+    void Start()
+    {
+        firstItem = false;
+        secondItem = false;
+        thirdItem = false;
+        fourthItem = false;
+        fifthItem = false;
+        sixthItem = false;
+
+        roomObjects.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

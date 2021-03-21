65421475c1dbff4c43c18a6fc4837d25d22aec6d
Dion Pimentel <dionpimentel260@gmail.com>
2021-03-20 19:35:14 -0700

    added interact

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,66 +4,103 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    /*
     private bool firstItem;
     private bool secondItem;
     private bool thirdItem;
     private bool fourthItem;
     private bool fifthItem;
     private bool sixthItem;
+    */
+
+    public static GameController instance;
 
     public GameObject roomObjects;
 
-    private AudioSource bgm;
+
+    [SerializeField]
+    private AudioSource BGM;
+    [SerializeField]
     private AudioSource dialogue;
 
     public SkinnedMeshRenderer[] coworkers;
 
     public MeshRenderer[] skins;
 
+    [Header("NPC Materials")]
+    public Material skinColor;
+    public Material pants;
+    public Material undershirt;
+    public Material shirt;
+    public Material belt;
+    public Material tie;
+
+
+    private Material[] coworkerMats = new Material[6];
+
+    private bool firstUpdate;
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
     void Start()
     {
-        firstItem = false;
-        secondItem = false;
-        thirdItem = false;
-        fourthItem = false;
-        fifthItem = false;
-        sixthItem = false;
 
         //roomObjects.SetActive(false);
 
-        coworkers = roomObjects.GetComponentsInChildren<SkinnedMeshRenderer>();
+        coworkerMats[0] = skinColor;
+        coworkerMats[1] = pants;
+        coworkerMats[2] = undershirt;
+        coworkerMats[3] = shirt;
+        coworkerMats[4] = belt;
+        coworkerMats[5] = tie;
 
-        foreach(SkinnedMeshRenderer skin in coworkers)
-        {
-            skin.materials = new Material[6];
-            skin.materials[0] = null;
-            skin.materials[1] = null;
-            skin.materials[2] = null;
-            skin.materials[3] = null;
-            skin.materials[4] = null;
-            skin.materials[5] = null;
-            
-        }
+        coworkers = roomObjects.GetComponentsInChildren<SkinnedMeshRenderer>();
 
         skins = roomObjects.GetComponentsInChildren<MeshRenderer>();
 
-        foreach(MeshRenderer mesh in skins)
-        {
-            mesh.materials = new Material[1];
-            mesh.materials[0] = null;
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!firstUpdate)
+        {
+            foreach (SkinnedMeshRenderer skin in coworkers)
+            {
+                skin.materials = new Material[6];
+                skin.materials[0] = null;
+                skin.materials[1] = null;
+                skin.materials[2] = null;
+                skin.materials[3] = null;
+                skin.materials[4] = null;
+                skin.materials[5] = null;
+
+            }
+
+            foreach (MeshRenderer mesh in skins)
+            {
+                mesh.materials = new Material[1];
+                mesh.materials[0] = null;
+            }
+            firstUpdate = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Colorize();
+        }
     }
 
     public void ActivateSound()
     {
-        
-        firstItem = true;
+
+
     }
 
     public void ShowRoom()
@@ -72,6 +109,32 @@ public class GameController : MonoBehaviour
     }
 
     public void Colorize()
+    {
+
+        foreach (SkinnedMeshRenderer skin in coworkers)
+        {
+            skin.materials = coworkerMats;
+
+        }
+
+        foreach (MeshRenderer mesh in skins)
+        {
+            if (mesh.gameObject.GetComponent<MaterialStorage>() != null)
+            {
+                mesh.materials = mesh.gameObject.GetComponent<MaterialStorage>().mat;
+            }
+
+        }
+
+
+    }
+
+    public void StartAI()
+    {
+
+    }
+
+    public void WinGame()
     {
 
     }
---
+++ Assets/Scripts/MaterialStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialStorage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //[HideInInspector]
+    public Material[] mat;
+
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().materials;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}

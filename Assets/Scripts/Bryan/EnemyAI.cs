5a65237dccdaf6e648c17a293b1ab13ba333c483
Destrayon <39275283+Destrayon@users.noreply.github.com>
2021-03-22 01:08:22 -0700

    Made AI Changes
    AI Changes.

--- Assets/Scripts/Bryan/EnemyAI.cs
+++ Assets/Scripts/Bryan/EnemyAI.cs
@@ -1,55 +1,119 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IInteractable
 {
-    [SerializeField]
-    private Transform target;
+    public float StunTime { get; private set; }
+    public bool IsStunned { get; private set; }
+    [SerializeField] private float maxDistance, minDistance, stunTime, stunCooldown, escapeDistance;
+    [SerializeField] private Transform target;
     private NavMeshAgent agent;
-    private float distance;
-    private float contactCooldown = 3;
-    private float period = 2.0f;
-
-    public int maxDistance;
-
-    [HideInInspector]
-    public Vector3 startPosition;
+    private float distance, cooldown;
+    private Action state;
+    private GameObject text;
 
     // Start is called before the first frame update
     void Awake()
     {
+        text = transform.Find("Stunned Text").gameObject;
         agent = GetComponent<NavMeshAgent>();
-        startPosition = transform.position;
+        state = SitIdle;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        distance = Vector3.Distance(target.position, transform.position);
+        if (IsStunned) state = Stunned;
 
-        if (distance > maxDistance)
+        if (cooldown > 0 && !IsStunned) cooldown -= Time.fixedDeltaTime;
+
+        distance = Vector3.Distance(target.position - new Vector3(0, target.position.y, 0), transform.position - new Vector3(0, transform.position.y, 0));
+
+        state();
+    }
+
+    private void SitIdle()
+    {
+        agent.destination = transform.position;
+
+        if (distance <= maxDistance)
         {
-            //if(Time.time > contactCooldown)
-            agent.isStopped = true; //agent.destination = new Vector3(1, 1, 1);
-            transform.LookAt(target);
-        }
-        else
-        {
-            agent.isStopped = false;
-            agent.destination = target.position;
+            state = MoveTowardsPlayer;
+            return;
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    private void MoveTowardsPlayer()
     {
-        Debug.Log(other.name);
-        if(other.name == "Player")
+        if (distance > escapeDistance)
         {
-            other.transform.position = startPosition;
-            //contactCooldown += period;
-
+            state = SitIdle;
+            return;
         }
+
+        if (distance <= minDistance)
+        {
+            state = AttackPlayer;
+            return;
+        }
+
+        agent.destination = target.position;
+    }
+
+    private void AttackPlayer()
+    {
+        agent.destination = transform.position;
+
+        transform.LookAt(target);
+
+        if (distance > minDistance + .5f)
+        {
+            state = MoveTowardsPlayer;
+            return;
+        }
+    }
+
+    private void Stunned()
+    {
+        if (StunTime <= 0)
+        {
+            IsStunned = false;
+            state = MoveTowardsPlayer;
+            text.SetActive(false);
+            return;
+        }
+
+        StunTime -= Time.fixedDeltaTime;
+
+        agent.destination = transform.position;
+    }
+
+    private void Stun()
+    {
+        if (cooldown <= 0)
+        {
+            IsStunned = true;
+            cooldown = stunCooldown;
+            StunTime = stunTime;
+            text.SetActive(true);
+        }
+    }
+
+    public void Interact()
+    {
+        Stun();
+    }
+
+    public float GetStunTimeSetting()
+    {
+        return stunTime;
+    }
+
+    public Transform GetTarget()
+    {
+        return target;
     }
 }
---
+++ Assets/StunAnimation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunAnimation : MonoBehaviour
+{
+    private Color originalColor;
+    
+    private EnemyAI ai;
+    private TextMesh textMesh;
+    [SerializeField] private float textMovementSpeed;
+    private float yOffset;
+    private void Awake()
+    {
+        ai = GetComponentInParent<EnemyAI>();
+        textMesh = GetComponent<TextMesh>();
+        originalColor = textMesh.color;
+        yOffset = transform.localPosition.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 aiPosition = ai.GetTarget().position - new Vector3(0, ai.GetTarget().position.y, 0);
+        Vector3 transformPosition = transform.position - new Vector3(0, transform.position.y, 0);
+        Vector3 direction = (aiPosition - transformPosition).normalized;
+
+        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+
+        transform.rotation = Quaternion.Euler(0, angle - 180, 0);
+    }
+
+    private void OnEnable()
+    {
+        textMesh.color -= new Color(0, 0, 0, 1);
+        transform.localPosition = new Vector3(0, yOffset, 0);
+        StartCoroutine(Animation());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(Animation());
+        textMesh.color = originalColor;
+    }
+
+    private IEnumerator Animation()
+    {
+        float GetValue(float x)
+        {
+            return Mathf.Abs(2 * x - 1);
+        }
+
+        float stunTime = ai.GetStunTimeSetting();
+
+        while (ai.StunTime > 0)
+        {
+            float value = GetValue(ai.StunTime / stunTime);
+
+            textMesh.color = originalColor - new Color(0, 0, 0, value);
+
+            textMesh.transform.position += new Vector3(0, textMovementSpeed * Time.fixedDeltaTime, 0);
+
+            yield return new WaitForSeconds(1f / 60f);
+        }
+
+        textMesh.color = originalColor - new Color(0, 0, 0, 1);
+    }
+}
